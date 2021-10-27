46915771bfd7bc7801cc0c74abf53cab9022b085
Nayan <nayan@appsmith.com>
2021-10-27 12:33:14 +0600

    -added super user check in send test email API

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/EnvManager.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/EnvManager.java
@@ -322,7 +322,7 @@ public class EnvManager {
     }
 
     public Mono<Boolean> sendTestEmail() {
-        return sessionUserService.getCurrentUser()
+        return verifyCurrentUserIsSuper()
                 .flatMap(user -> emailSender.sendMail(
                         user.getEmail(),
                         "Test email from Appsmith",
